44d7bd59de18540b190819608631b66cb7a8ca07
Valentýna <123517319+entyna@users.noreply.github.com>
2024-12-07 15:40:58 +0100

    fix: new Contents keys

--- database/seeders/ContentsTableSeeder.php
+++ database/seeders/ContentsTableSeeder.php
@@ -13,9 +13,9 @@ class ContentsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        Content::create(['key' => 'heading', 'lang' => 'cs', 'value' => 'Vánoční otevírací doba']);
-        Content::create(['key' => 'heading', 'lang' => 'en', 'value' => 'Holiday Opening Hours']);
-        Content::create(['key' => 'text', 'lang' => 'cs', 'value' => 'Lákamí vůněhulás úmyval rohlivý jednovod lek an školivé sůl bájedpodl roští. Povačkoje hanalemi jenžto čuvad málně lehraží jakočil zásou k jednovátc tavětraje.']);
-        Content::create(['key' => 'text', 'lang' => 'en', 'value' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.']);
+        Content::create(['key' => 'infobox-heading', 'lang' => 'cs', 'value' => 'Vánoční otevírací doba']);
+        Content::create(['key' => 'infobox-heading', 'lang' => 'en', 'value' => 'Holiday Opening Hours']);
+        Content::create(['key' => 'infobox-text', 'lang' => 'cs', 'value' => 'Lákamí vůněhulás úmyval rohlivý jednovod lek an školivé sůl bájedpodl roští. Povačkoje hanalemi jenžto čuvad málně lehraží jakočil zásou k jednovátc tavětraje.']);
+        Content::create(['key' => 'infobox-text', 'lang' => 'en', 'value' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.']);
     }
 }
