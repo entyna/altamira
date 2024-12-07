0220c3f6fcb037cd9b515a01daf0198b34e5723b
Valentýna <123517319+entyna@users.noreply.github.com>
2024-12-07 15:41:40 +0100

    DashboardController moved to Admin Folder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
